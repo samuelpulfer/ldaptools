4565a32cb0d6c7e5766d1941e67f5ef18f80797e
wunderlins <simon.wunderlin@usb.ch>
2015-11-25 10:08:05 +0100

    finished full config

--- etc/wus-desktop5.py
+++ etc/wus-desktop5.py
@@ -7,35 +7,73 @@ modification_logfile="var/modification.log"
 
 # groups to sync
 sync = [
+	# Ärzte
 	{
 		"from": "(&(cn=MQ_ANA_LA)(objectClass=group))", 
-		"to": "CN=MQ_B_U_Anaesthesiologie-VD-User-Aerzte-LA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
+		"to": "CN=MQ_B_U_Anaesthesiologie-Aerzte-LA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
 		"method": "sync", # sync|copy|delete
 	},
 	{
 		"from": "(&(cn=MQ_ANA_OA)(objectClass=group))", 
-		"to": "CN=MQ_B_U_Anaesthesiologie-VD-User-Aerzte-OA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
+		"to": "CN=MQ_B_U_Anaesthesiologie-Aerzte-OA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
 		"method": "sync", # sync|copy|delete
 	},
 	{
 		"from": "(&(cn=MQ_ANA_AA)(objectClass=group))", 
-		"to": "CN=MQ_B_U_Anaesthesiologie-VD-User-Aerzte-AA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
+		"to": "CN=MQ_B_U_Anaesthesiologie-Aerzte-AA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
 		"method": "sync", # sync|copy|delete
 	},
+	{
+		"from": "(&(cn=MQ_ANA_OIB_Aerzte)(objectClass=group))", 
+		"to": "CN=MQ_B_U_Anaesthesiologie-Aerzte-OIB,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
+		"method": "sync", # sync|copy|delete
+	},
+	
+	# Pflege
+	{
+		"from": "(&(cn=MQ_ANA_Pflege)(objectClass=group))", 
+		"to": "CN=MQ_B_U_Anaesthesiologie-Pflege-ANA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
+		"method": "sync", # sync|copy|delete
+	},
+	{
+		"from": "(&(cn=MQ_ANA_OIB_Pflege)(objectClass=group))", 
+		"to": "CN=MQ_B_U_Anaesthesiologie-Pflege-OIB,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
+		"method": "sync", # sync|copy|delete
+	},
+	{
+		"from": "(&(cn=MQ_ANA_OPS)(objectClass=group))", 
+		"to": "CN=MQ_B_U_Anaesthesiologie-Pflege-OPS,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
+		"method": "sync", # sync|copy|delete
+	},
+	
+	# Admin
+	{
+		"from": "(&(cn=MQ_ANA_IT)(objectClass=group))", 
+		"to": "CN=MQ_B_U_Anaesthesiologie-Admin-IT,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
+		"method": "sync", # sync|copy|delete
+	},
+	{
+		"from": "(&(cn=MQ_ANA_SEK)(objectClass=group))", 
+		"to": "CN=MQ_B_U_Anaesthesiologie-Admin-Sek,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch",
+		"method": "sync", # sync|copy|delete
+	},
+	
+
+
 ]
 
 """
 	{
 		"from": "CN=MQ_ANA_LA,OU=Exchange_Adressbuecher,OU=PITServer,DC=ms,DC=uhbs,DC=ch", 
-		"to": "CN=MQ_B_U_Anaesthesiologie-VD-User-Aerzte-LA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch"
+		"to": "CN=MQ_B_U_Anaesthesiologie-Aerzte-LA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch"
 	},
 	{
 		"from": "CN=MQ_ANA_OA,OU=Exchange_Adressbuecher,OU=PITServer,DC=ms,DC=uhbs,DC=ch", 
-		"to": "CN=MQ_B_U_Anaesthesiologie-VD-User-Aerzte-OA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch"
+		"to": "CN=MQ_B_U_Anaesthesiologie-Aerzte-OA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch"
 	},
 	{
 		"from": "CN=MQ_ANA_AA,OU=Exchange_Adressbuecher,OU=PITServer,DC=ms,DC=uhbs,DC=ch", 
-		"to": "CN=MQ_B_U_Anaesthesiologie-VD-User-Aerzte-AA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch"
+		"to": "CN=MQ_B_U_Anaesthesiologie-Aerzte-AA,OU=Business,OU=Groups,OU=MQInf,OU=USB,DC=ms,DC=uhbs,DC=ch"
 	},
 """
 
